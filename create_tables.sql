e0e4e322cb435c1cb04f2141b6253689d3ac91f8
George Ruck <georgeadruck@gmail.com>
2023-01-27 14:03:43 +0100

    Create data tables and insert data

---
+++ create_tables.sql
@@ -0,0 +1,16 @@
+-- Create first table
+CREATE TABLE photoinfo(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    pathfile VARCHAR(1024) NOT NULL,
+    photoname VARCHAR(50) NOT NULL
+);
+
+-- Create second table where photo information save
+-- Second table unecessary, just to show relation between tables
+-- Grayscale images 64*64 converted to string
+
+-- NOTE - it is never recommended that you save the image IN the database
+CREATE TABLE photos_grayscale_64(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    photos VARCHAR(4096)
+);
---
+++ insert_photoinfo.sql
@@ -0,0 +1,20 @@
+INSERT INTO photoinfo (
+    pathfile,
+    photoname
+)
+VALUES ('C:/Users/Admin/Documents/Python Scripts/Postgres/Pictures/IMG-20220219-WA0039.jpg','Louis Climbing');
+INSERT INTO photoinfo (
+    pathfile,
+    photoname
+)
+VALUES ('C:/Users/Admin/Documents/Python Scripts/Postgres/Pictures/IMG-20220220-WA0007.jpg','Aiguille du Plan');
+INSERT INTO photoinfo (
+    pathfile,
+    photoname
+)
+VALUES ('C:/Users/Admin/Documents/Python Scripts/Postgres/Pictures/IMG-20220220-WA0041.jpg','SteepbyStep');
+INSERT INTO photoinfo (
+    pathfile,
+    photoname
+)
+VALUES ('C:/Users/Admin/Documents/Python Scripts/Postgres/Pictures/IMG-20220220-WA0043.jpg','Roped Up');
