65c88f009d6088ae9ec4fc1086f9933ccd09c704
George Ruck <georgeadruck@gmail.com>
2023-02-16 15:32:58 +0100

    Add data to existing Table via psql

--- readdata.py
+++ readdata.py
@@ -4,14 +4,20 @@
 import psycopg2
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 
 import cv2
+
+def plot_grayscale_psql(df,index):
+    
+    image = np.array(df.loc[df['id'] == index,'photos'].iloc[0].split(','))
+    image = np.array(np.reshape(image,(int(len(image)**0.5),)*2),dtype=np.uint8)
+    cv2.imshow('image',image)
 
 
 if __name__ == "__main__":
     
-    #pw = #define password for db
+    pw = input('Password:') #input postgres db
     
     #connect to database - my db is at port 5433
     conn = psycopg2.connect(dbname = "pythontest", user = "postgres", password = pw, port=5433)
@@ -21,10 +27,8 @@ if __name__ == "__main__":
     #cur.execute("SELECT * FROM photoinfo")
     #cur.close()
     
-    #simple method with open connection
+    #Simple read with open db connection
     photoinfo_df = pd.read_sql_query('SELECT * FROM "photoinfo"', conn)
-    
-    conn.close()
     
     #%%
     output_df = pd.DataFrame(index = photoinfo_df.index,columns = ['id','photos'])
@@ -39,5 +43,17 @@ if __name__ == "__main__":
         
         #write to string
         output_df.iloc[i] = [i+1,np.array2string(img.flatten(),separator = ',',threshold = 50000).strip('[]').replace('\n', '').replace(' ', '')]
-        
-    #write to .sql file to input into table
+    
+    #reopen connection with sqlalchemy
+    db = create_engine("postgresql+psycopg2://postgres:{}@localhost:5433/pythontest".format(pw))
+    
+    #write to .sql file to input into table: photos_grayscale_64
+    output_df.to_sql('photos_grayscale_64',db,if_exists='replace',index=False)
+    
+    #%%
+    #Check written table and plot images
+    photos_gs = pd.read_sql_query('SELECT * FROM "photos_grayscale_64"', conn)
+    conn.close()
+    
+    #Plot image using grayscale for check
+    plot_grayscale_psql(photos_gs,1)    
